674e59fbc2d5fdbeeccbae327f6793369d345658
Adebayo Oluwadamilola <goshensoftinc@gmail.com>
2019-02-03 13:37:26 +0100

    setup roles entity

---
+++ src/roles/database/roles.entity.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by adebayooluwadamilola on 2/3/19.
+ */
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn} from 'typeorm';
+
+@Entity('roles')
+export class Role {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('varchar', { length: 200 })
+    name: string;
+
+    @Column('text')
+    description: string;
+
+    @CreateDateColumn()
+    createdAt: string;
+
+    @UpdateDateColumn()
+    updatedAt: string;
+}
